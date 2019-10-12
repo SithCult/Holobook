4fe20b54f37f583a47526dcb795cb076e586a9fe
Christian Aichner <contact@aichner-christian.com>
2019-10-12 21:43:27 +0200

    Improve ProfilePage
    A profile, post and alert card now exist.

--- src/components/pages/ProfilePage/index.jsx
+++ src/components/pages/ProfilePage/index.jsx
@@ -5,6 +5,8 @@ import React from 'react';
 //> Additional modules
 // Fade In Animation
 import FadeIn from 'react-fade-in';
+// Country list
+import countryList from 'react-select-country-list';
 
 //> Redux
 // Connect
@@ -18,6 +20,15 @@ import {
   MDBContainer,
   MDBRow,
   MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardUp,
+  MDBAvatar,
+  MDBAlert,
+  MDBBtn,
+  MDBBadge,
+  MDBInput,
+  MDBIcon,
 } from 'mdbreact';
 
 //> Components
@@ -35,27 +46,232 @@ class ProfilePage extends React.Component {
   }
 
   state = {
-    
+    post_charlength: 0,
+    post: "",
   };
 
   componentDidMount = () => {
     
   }
 
-  render() {
-    const { auth } = this.props;
+  changeTextareaHandler = event => {
+    event.target.style.overflow = 'hidden';
+    event.target.style.height = 0;
+    event.target.style.height = event.target.scrollHeight + 'px';
 
-    console.log(auth);
+
+
+    if(event.target.value.length <= 500){
+      this.setState({
+        post: event.target.value
+      })
+    }
+  }
+
+  getCountry = (address) => {
+    let country = address ? countryList().getLabel(address.country) : null;
+    return country;
+  }
+
+  _renderBadge = (badge, key) => {
+    switch (badge.toLowerCase()) {
+      case "founder":
+        return (
+          <MDBCol key={key}>
+            <MDBBadge pill color="elegant-color">
+            <MDBIcon icon="fire" className="pr-2"/>
+            Founder
+            </MDBBadge>
+          </MDBCol>
+        )
+        break;
+      case "member":
+        return (
+          <MDBCol key={key}>
+            <MDBBadge pill color="red">
+            <MDBIcon icon="user" className="pr-2"/>
+            Member
+            </MDBBadge>
+          </MDBCol>
+        )
+        break;
+      case "hand":
+        return (
+          <MDBCol key={key}>
+            <MDBBadge pill color="gold">
+            <MDBIcon fab icon="sith" className="pr-2"/>
+            Hand of the Emperor
+            </MDBBadge>
+          </MDBCol>
+        )
+        break;
+      default:
+        break;
+    }
+  }
+
+  getBadges = (badges) => {
+    let result = "";
+
+    if(badges){
+      result = badges.map((badge, key) => {
+        return this._renderBadge(badge, key)
+      });
+    }
+
+    if(result === ""){
+      return null;
+    } else {
+      return <MDBRow className="text-center badge-row">{result}</MDBRow>;
+    }
+  }
+
+  render() {
+    const { auth, profile } = this.props;
+
+    console.log(auth, profile);
 
     return (
-      null
+      <MDBContainer id="profile" className="pt-5 mt-5">
+        <MDBRow>
+          <MDBCol md="3">
+            <MDBCard testimonial>
+              <MDBCardUp className='red' />
+              <MDBAvatar className='mx-auto white'>
+                <img
+                  src='https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg'
+                  alt=''
+                />
+              </MDBAvatar>
+              <MDBCardBody>
+                <p className="lead font-weight-bold mb-0">{profile.title} {profile.sith_name}</p>
+                <p className="text-muted">{this.getCountry(profile.address)}</p>
+                {this.getBadges(profile.badges)}
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md="6">
+            <MDBCard>
+              <MDBCardBody>
+                <h3>Greetings, {profile.title} {profile.sith_name}</h3>
+                <MDBInput 
+                type="textarea"
+                label="What's on your mind?"
+                name="post"
+                outline
+                value={this.state.post}
+                onChange={this.changeTextareaHandler}
+                />
+                <p>
+                  <div className="d-inline post-settings">
+                    <span>
+                      <MDBIcon icon="language" size="lg" />
+                    </span>
+                    <span>
+                      <MDBIcon far icon="eye" size="lg" />
+                    </span>
+                  </div>
+                  <small
+                  className={this.state.post.length === 500 ? "text-danger float-right" : "float-right"}
+                  >
+                  {this.state.post.length} / 500
+                  </small>
+                </p>
+                <div className="clearfix"/>
+                <hr/>
+                <div className="actions">
+                  <MDBBtn
+                  color="elegant"
+                  rounded
+                  >
+                  <MDBIcon icon="image" className="pr-2" size="lg" />
+                  Photo
+                  </MDBBtn>
+                  <MDBBtn
+                  color="elegant"
+                  rounded
+                  >
+                  <MDBIcon far icon="smile" className="pr-2" size="lg" />
+                  Feeling
+                  </MDBBtn>
+                  <MDBBtn
+                  color="elegant"
+                  rounded
+                  >
+                  <MDBIcon icon="user-plus" className="pr-2" size="lg" />
+                  Tag
+                  </MDBBtn>
+                </div>
+                {this.state.post.length > 0 &&
+                <FadeIn>
+                <div className="text-right">
+                  {(profile.title.toLowerCase() === "darth") &&
+                    <MDBBtn
+                    color="red"
+                    rounded
+                    outline
+                    >
+                      <MDBIcon fab icon="sith" className="pr-2" size="lg" />
+                      Post als SithCult
+                    </MDBBtn>
+                  }
+                  <MDBBtn
+                  color="elegant"
+                  rounded
+                  >
+                    <MDBIcon icon="paper-plane" className="pr-2" size="lg" />
+                    Post
+                  </MDBBtn>
+                </div>
+                </FadeIn>
+                }
+              </MDBCardBody>
+            </MDBCard>
+            {false &&
+            <MDBAlert color="danger" className="my-2">
+              <h4 className="alert-heading">Directive</h4>
+              <MDBRow>
+                <MDBCol md="auto" className="align-self-center">
+                  <MDBIcon icon="exclamation-triangle" size="2x" />
+                </MDBCol>
+                <MDBCol>
+                  <p className="m-0">
+                  To the weapons! The Jedi have bombed the Imperial Base Omega-Theta on Balmorra. 
+                  Report to your local chief of operations!
+                  </p>
+                </MDBCol>
+              </MDBRow>
+            </MDBAlert>
+            }
+            <MDBAlert color="success" className="my-2">
+              <MDBRow>
+                <MDBCol>
+                  <h4 className="alert-heading">Welcome to our closed Beta!</h4>
+                  <p>Please note, that this is an <strong>early Beta version</strong> of Holobook. 
+                  Only certain features are working - some are not yet functional.</p>
+                </MDBCol>
+                <MDBCol md="auto" className="align-self-center">
+                  <MDBBtn color="success" rounded>
+                    <MDBIcon icon="check" />
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+            </MDBAlert>
+          </MDBCol>
+          <MDBCol md="3">
+
+          </MDBCol>
+
+        </MDBRow>
+      </MDBContainer>
     );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
   }
 }
 
