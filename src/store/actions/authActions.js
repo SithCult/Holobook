ff9af3c08dfda4b548c77d7615bf0beca4ae4b01
Christian Aichner <contact@aichner-christian.com>
2019-10-12 16:23:03 +0200

    Improve actions
    The authActions now work as intended. The user
    is no longer created if the name is duplicate.

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -37,32 +37,58 @@ export const signUp = (newUser) => {
 
         console.log(newUser);
 
-        // Create new user to firebase
-        firebase.auth().createUserWithEmailAndPassword(
-            newUser.email,
-            newUser.password
-        ).then((response) => {
-            // Create data for user we just created
-            return firestore.collection('users').doc(response.user.uid).set({
-                full_name: newUser.full_name,
-                email: newUser.email,
-                tracking: newUser.tracking,
-                details: newUser.details,
-                newsletter: newUser.newsletter,
-                letter: newUser.letter,
-                address: newUser.address,
-                law: newUser.law
-            })
-        }).then(() => {
-            dispatch({
-                type: 'SIGNUP_SUCCESS'
-            })
-        }).catch((err) => {
-            dispatch({
-                type: 'SIGNUP_ERROR',
-                err
-            })
-        })
+        let test = 1;
+
+        // Check for Sithname duplicates
+        firestore.collection('users').get()
+        .then((querySnapshot) => {
+            let result = undefined;
+            let duplicate = false;
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                result = doc.data();
+                if(result){
+                    if(result.sith_name === newUser.sith_name){
+                        duplicate = true;
+                        // Match found
+                        dispatch({
+                            type: 'SIGNUP_DUPLICATE'
+                        });
+                    }
+                }
+            });
+
+            if(!duplicate){
+                // Create new user to firebase
+                firebase.auth().createUserWithEmailAndPassword(
+                    newUser.email,
+                    newUser.password
+                ).then((response) => {
+                    // Create data for user we just created
+                    return firestore.collection('users').doc(response.user.uid).set({
+                        full_name: newUser.full_name,
+                        sith_name: newUser.sith_name,
+                        email: newUser.email,
+                        tracking: newUser.tracking,
+                        details: newUser.details,
+                        newsletter: newUser.newsletter,
+                        letter: newUser.letter,
+                        address: newUser.address,
+                        law: newUser.law
+                    })
+                }).then(() => {
+                    dispatch({
+                        type: 'SIGNUP_SUCCESS'
+                    })
+                }).catch((err) => {
+                    dispatch({
+                        type: 'SIGNUP_ERROR',
+                        errCode: 1,
+                        err
+                    })
+                })
+            }
+        });
     }
 }
 
