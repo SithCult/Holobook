7f515cd2ab924f62c6bbd558975d9faafe06ae1d
Christian Aichner <contact@aichner-christian.com>
2019-10-13 23:12:09 +0200

    Improve navigation
    The navigation bar has been improved. For signed in
    users the bar has been totally redesigned.

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -16,73 +16,115 @@ import {
     MDBIcon,
 } from 'mdbreact';
 
+//> Redux
+// Connect
+import { connect } from 'react-redux';
+// Actions
+import { signOut } from '../../../store/actions/authActions';
+
 //> Images
 // Logo
 import IMGlogo from '../../../assets/images/logo_white_sm.png';
 
 
 class Navbar extends React.Component{
-    state = {
-        collapseID: ""
-    };
+  state = {
+    collapseID: ""
+  };
 
-    toggleCollapse = (collapseID) => () =>
-        this.setState((prevState) => ({
-        collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-        }));
+  toggleCollapse = (collapseID) => () =>
+    this.setState((prevState) => ({
+    collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+    }));
 
-    closeCollapse = (collapseID) => () => {
-        window.scrollTo(0, 0);
-        this.state.collapseID === collapseID && this.setState({ collapseID: "" });
-    };
+  closeCollapse = (collapseID) => () => {
+    window.scrollTo(0, 0);
+    this.state.collapseID === collapseID && this.setState({ collapseID: "" });
+  };
 
-    render(){
-        const overlay = (
-        <div
-            id="sidenav-overlay"
-            style={{ backgroundColor: "transparent" }}
+  render(){
+    const overlay = (
+    <div
+      id="sidenav-overlay"
+      style={{ backgroundColor: "transparent" }}
+      onClick={this.toggleCollapse("mainNavbarCollapse")}
+    />
+    );
+
+    const { collapseID } = this.state;
+
+    const { auth, profile } = this.props;
+
+    console.log(profile);
+
+    return(
+      <div>
+        <MDBNavbar color="sithcult-dark" dark expand="md" fixed="top" scrolling>
+          <MDBContainer fluid={auth.uid}>
+            <MDBNavbarBrand href={!auth.uid ? "/" : "/holobook"} className="py-0 font-weight-bold">
+              <img src={IMGlogo} height="50px" alt="SithCult Logo"/>
+            </MDBNavbarBrand>
+            <MDBNavbarToggler
             onClick={this.toggleCollapse("mainNavbarCollapse")}
-        />
-        );
-
-        const { collapseID } = this.state;
-        return(
-            <div>
-                <MDBNavbar color="sithcult-dark" dark expand="md" fixed="top" scrolling>
-                <MDBContainer>
-                    <MDBNavbarBrand href="/" className="py-0 font-weight-bold">
-                        <img src={IMGlogo} height="50px" alt="SithCult Logo"/>
-                    </MDBNavbarBrand>
-                    <MDBNavbarToggler
-                    onClick={this.toggleCollapse("mainNavbarCollapse")}
-                    />
-                    <MDBCollapse
-                    id="mainNavbarCollapse"
-                    isOpen={this.state.collapseID}
-                    navbar
-                    >
-                    <MDBNavbarNav right>
-                        <MDBNavItem>
-                            <MDBBtn 
-                            color="modern"
-                            >
-                            <MDBIcon icon="key" className="pr-2" />
-                            Already member? Log in
-                            </MDBBtn>
-                        </MDBNavItem>
-                    </MDBNavbarNav>
-                    </MDBCollapse>
-                </MDBContainer>
-            </MDBNavbar>
-            {collapseID && overlay}
-            </div>
-        );
-    }
+            />
+            <MDBCollapse
+            id="mainNavbarCollapse"
+            isOpen={this.state.collapseID}
+            navbar
+            >
+            <MDBNavbarNav right className="text-white">
+            {!auth.uid &&
+              <MDBNavItem>
+                <MDBBtn 
+                color="modern"
+                >
+                <MDBIcon icon="key" className="pr-2" />
+                Already member? Log in
+                </MDBBtn>
+              </MDBNavItem>
+            }
+            {auth.uid &&
+              <>
+                {profile.credits &&
+                  <span>
+                    <MDBIcon fab icon="sith" className="pr-2 orange-text" />
+                    {profile.credits.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.")}
+                  </span>
+                }
+                {profile.reputation &&
+                  <span className="ml-3">
+                    <MDBIcon icon="medal" className="pr-2 purple-text" />
+                    {profile.reputation.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.")}
+                  </span>
+                }
+              </>
+            }
+            </MDBNavbarNav>
+            </MDBCollapse>
+          </MDBContainer>
+      </MDBNavbar>
+      {collapseID && overlay}
+      </div>
+    );
+  }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Navbar);
 
 /** 
  * SPDX-License-Identifier: (EUPL-1.2)
- * Copyright © 2019 Werbeagentur Christian Aichner
+ * Copyright © 2019 Christian Aichner
  */
