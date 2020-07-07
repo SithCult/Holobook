a9ae5d3d556759b8cee87936cbab2c3934d65868
Christian Aichner <contact@aichner-christian.com>
2020-07-07 10:09:49 +0200

    Improve RankItem
    The design has been changed. Imperial basic has been added.

--- src/components/atoms/RankItem/index.jsx
+++ src/components/atoms/RankItem/index.jsx
@@ -45,18 +45,19 @@ class RankItem extends React.Component {
         <div className="clickable name d-inline ml-1">{this.getRank(rank)}</div>
         <div>
           <MDBPopoverHeader className="flex-center">
-            <div>Rank information</div>
+            <div className="text-dark basic hand">Rank information</div>
           </MDBPopoverHeader>
-          <MDBPopoverBody className="text-white">
+          <MDBPopoverBody>
             <div>
               {(() => {
                 switch (rank) {
                   case "hand":
                     return (
                       <div>
-                        <p className="font-weight-bold mb-2">
+                        <p className="lead font-weight-bold mb-0">
                           Hand of the Emperor
                         </p>
+                        <p className="mb-2 basic">Hand of the Emperor</p>
                         <p className="text-muted mb-1">
                           The Hand of the Emperor is the enforcer of our
                           Emperor's commands and visions. The highest political
@@ -67,7 +68,8 @@ class RankItem extends React.Component {
                   case "moff":
                     return (
                       <div>
-                        <p className="font-weight-bold mb-2">Moff</p>
+                        <p className="lead font-weight-bold mb-0">Moff</p>
+                        <p className="mb-2 basic">Moff</p>
                         <p className="text-muted mb-1">
                           A moff is the representative of a cluster (country)
                           and has the sole power over all members in this area.
@@ -78,7 +80,8 @@ class RankItem extends React.Component {
                   case "grandmoff":
                     return (
                       <div>
-                        <p className="font-weight-bold mb-2">Grandmoff</p>
+                        <p className="lead font-weight-bold mb-0">Grandmoff</p>
+                        <p className="mb-2 basic">Grandmoff</p>
                         <p className="text-muted mb-1">
                           A Grandmoff is the representative of a network of
                           clusters. He has the sole power over all members in
